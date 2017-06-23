ea088d6e28596c631f6667e4b01949783003c1f1
Chao Luo (chaoluo) <chaoluo@cisco.com>
2017-06-23 09:18:30 +0800

    feat: update metrics health check

--- src/main/java/com/chaoluo/helloworld/Application.java
+++ src/main/java/com/chaoluo/helloworld/Application.java
@@ -1,26 +1,64 @@
 package com.chaoluo.helloworld;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.concurrent.TimeUnit;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+@RestController
 @EnableAutoConfiguration
 public class Application {
 
-    @RequestMapping(value = "/resources", method = RequestMethod.GET)
-    @ResponseBody
-    String getResources() {
-        return "Get Resources";
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+
+        };
     }
 
-    @RequestMapping(value = "/resources", method = RequestMethod.POST, consumes = {"application/json"})
-    @ResponseBody
-    String postResources() {
-        return "Post Resources!";
+    @Bean
+    MetricRegistry metricRegistry() {
+        return new MetricRegistry();
+    }
+
+    @Bean
+    ConsoleReporter consoleReporter() {
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry())
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(5, TimeUnit.SECONDS);
+        meter().mark();
+        return reporter;
+    }
+
+    @Bean
+    Timer responses() {
+        final Timer responses = metricRegistry().timer(name(Application.class, "responses"));
+        return responses;
+    }
+
+    @Bean
+    Meter meter() {
+        return metricRegistry().meter("requests");
+    }
+
+    @GetMapping("/")
+    public String get() {
+        final Timer.Context context = responses().time();
+        meter().mark();
+        context.stop();
+        return "hello,world";
     }
 
     public static void main(String[] args) throws Exception {
